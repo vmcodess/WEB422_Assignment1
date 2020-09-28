e75750cbb9217493bb944522d8912e46430e5109
vmatveev <vmatveevspam@gmail.com>
2020-09-28 13:02:56 -0400

    First commit

---
+++ server.js
@@ -0,0 +1,132 @@
+// Author : Vladislav Matveev
+
+//----------------
+// .env
+require('dotenv').config()
+//----------------
+
+const express = require("express");
+const cors = require("cors");
+//const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const dataService = require("./modules/data-service.js");
+
+const myData = dataService(process.env.MONGO_DB_URL);
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+// This tells express to make form data available via req.body in every request
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const HTTP_PORT = process.env.PORT || 8080;
+
+// ************* API Routes
+
+// POST /api/sales (NOTE: This route must read the contents of the request body) // addNewSale 
+app.post("/api/sales", async (req, res) => {
+
+ await myData.addNewSale().then(results => { 
+        res.status(200).send(results) 
+    }).catch(err => { 
+        res.status(500).send(err) 
+    })
+
+});
+
+/**
+ * EXAMPLE
+ */
+
+// app.get("api/example", async (req, res) => { 
+
+//  const data = {data: "test"}
+//  await res.status(200).send(data)
+
+// })
+
+// app.post("api/example", async (req, res) => { 
+
+// const data = req.body 
+// await res.status(200).send(data)
+   
+// })
+   
+/** 
+ * END EXAMPLE 
+ */
+// GET /api/sales (NOTE: This route must accept the numeric query parameters "page" and "perPage", ie: /api/sales?page=1&perPage=5 )    // getAllSales
+
+app.get("/api/sales", async (req,res) => {
+
+     await myData.getAllSales(1, 100).then(results => {
+        res.status(200).send(results)
+    }).catch(err => {
+        res.status(500).send(err)
+    })
+});
+
+app.get("/api/sales/:page/:perPage", async (req,res) => {
+
+    let page = req.params.page;
+    let perPage = req.params.perPage;
+
+    await myData.getAllSales(page, perPage).then(results => {
+        res.status(200).send(results)
+    }).catch(err => {
+        res.status(500).send(err)
+    })
+});
+
+
+// GET /api/sales (NOTE: This route must accept a numeric route parameter, ie: /api/sales/5bd761dcae323e45a93ccfe8) // getSalebyId
+app.get("/api/sales/:id", async (req, res) => {
+
+    let id = req.params.id;
+
+    await myData.getSaleById(id).then(results => { 
+        res.status(200).send(results) 
+    }).catch(err => { 
+        res.status(500).send(err) 
+    })
+});
+
+
+// PUT /api/sales (NOTE: This route must accept a numeric route parameter, ie: /api/sales/5bd761dcae323e45a93ccfe8 as well as read the contents of the request body) // updateSaleById
+app.put("/api/sales/:id", async (req, res) => {
+    let id = req.params.id;
+    let data = req.body
+    await myData.updateSaleById(data, id).then(results => { 
+        res.status(200).send(results) 
+    }).catch(err => { 
+        res.status(500).send(err) 
+    })
+});
+
+
+// DELETE /api/sales (NOTE: This route must accept a numeric route parameter, ie: /api/sales/5bd761dcae323e45a93ccfe8)  //deleteSale
+app.delete("/api/sales/:id", async (req, res) => {
+    let id = req.params.id;
+    console.log(req);
+
+    await myData.deleteSaleById(id).then(results => { 
+        res.status(200).send(results) 
+    }).catch(err => { 
+        res.status(500).send(err) 
+    })
+});
+
+
+
+// ************* Initialize the Service & Start the Server
+
+myData.initialize().then(()=>{
+    app.listen(HTTP_PORT,()=>{
+        console.log(`server listening on: ${HTTP_PORT}`);
+    });
+}).catch((err)=>{
+    console.log(err);
+});
+
