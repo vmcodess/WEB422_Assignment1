85fff5b1c3967cec87c869df4ccf8247b77a891f
vmatveev <vmatveevspam@gmail.com>
2020-09-28 14:05:58 -0400

    added decleration top of server.js

--- server.js
+++ server.js
@@ -1,4 +1,14 @@
-// Author : Vladislav Matveev
+/*********************************************************************************
+* WEB422 – Assignment 1
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy.
+* No part of this assignment has been copied manually or electronically from any other source
+* (including web sites) or distributed to other students.
+*
+* Name: Vladislav Matveev Student ID:032112138 Date: September 28, 2020
+* Heroku Link: https://web422-assmt1.herokuapp.com/
+* *** just add api/sales
+*
+********************************************************************************/
 
 //----------------
 // .env
